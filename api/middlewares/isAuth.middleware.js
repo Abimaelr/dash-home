86931475b512207c7029e07c6b1da47475350998
Abimael Albuquerque <abimaelr.albu@gmail.com>
2024-05-05 00:50:15 -0400

    feat: Device ID

--- api/middlewares/isAuth.middleware.js
+++ api/middlewares/isAuth.middleware.js
@@ -1,9 +1,29 @@
 
-const isAuthenticated = (req, res, next) => {
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next 
+ */
+export const isAuthenticated = (req, res, next) => {
     const Authorization = req.headers.Authorization;
 
     if (!Authorization) {
         throw new Error('You must be authenticated')
     }
+}
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next 
+ */
+export const validSensor = (req, res, next) => {
+    const code = req.header.DeviceId;
+
+     if (!code) {
+        return res.status(401).json({error: 'You not allowed to post'})
+    }
+
+    next();
 
 }
