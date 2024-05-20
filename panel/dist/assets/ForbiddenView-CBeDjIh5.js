9ab1e0d9d75e0b68b452a1d7c9fe56bed13b1a8a
Abimael Albuquerque <abimaelr.albu@gmail.com>
2024-05-20 17:32:40 -0400

    deploy

--- panel/dist/assets/ForbiddenView-CBeDjIh5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-C10fp8eh.js";const n={};function r(s,t){return c(),o("h1",null,"You Cannot access this resource")}const _=e(n,[["render",r]]);export{_ as default};
