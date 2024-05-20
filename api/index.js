8baddd4abc36deb5611e8b92f6377a7aa570a3a5
root <abimaelr.albu@gmail.com>
2024-05-20 21:05:31 +0000

    avoid multiple req

--- api/index.js
+++ api/index.js
@@ -6,9 +6,18 @@ import LoginRouter from './modules/login/index.js';
 import ReportRouter from './modules/report/index.js';
 import InstallationRouter from './modules/installation/index.js';
 
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100, // Limita cada IP a 100 requisições por janela (aqui, 15 minutos)
+  message: 'Muitas requisições criadas a partir deste IP, por favor, tente novamente após algum tempo.'
+});
+
+
 
 const app = express()
-
+app.use(limiter)
 app.use(express.json())
 app.use(cors())
 
